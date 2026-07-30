618a83e6cb0100804b0ad53f0874fb4a72787ee8
maheswari-scs-pg <maheswari@rvsgroup.com>
2026-07-30 14:48:56 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,15 +1,16 @@
 -- Create Database
 
 -- Write your query here
-
+create database CollegeDB
 
 -- Use Database
 
-
+use CollegeDB
 -- Create Department Table
 
-
+create table Department(Dept_Id int(5) primary key,Dept_Name varchar(25),Hod varchar(20));
 -- Insert Records
-
+insert into Department values(101,DS,Dr.Grace)
+insert into Department values(102,BCA,Dr.Padma)
 
 -- Display Department Details
