4746f49c3cd6db71b479330cc742163398ecc334
maheswari-scs-pg <maheswari@rvsgroup.com>
2026-07-30 15:26:55 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -8,3 +8,9 @@ CREATE TABLE Department
  DepartmentName VARCHAR(20),
  HOD VARCHAR(20)
 );
+
+INSERT INTO Department
+VALUES
+(101,'Computer Science','Dr.Ravi'),
+(102,'Commerce','Dr.Kumar'),
+(103,'Mathematics','Dr.Sudha');
