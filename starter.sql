582d805b8a501809d17a66cee03feb3ea6bb3cbb
maheswari-scs-pg <maheswari@rvsgroup.com>
2026-07-30 15:26:30 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,26 +1,10 @@
--- Create Database
-CREATE DATABASE IF NOT EXISTS CollegeDB;
+CREATE DATABASE CollegeDB;
 
-
--- Select Database
 USE CollegeDB;
 
-
--- Create Department Table
 CREATE TABLE Department
 (
-    DepartmentID INT(5) PRIMARY KEY,
-    DepartmentName VARCHAR(20),
-    HOD VARCHAR(20)
+ DepartmentID INT(5) PRIMARY KEY,
+ DepartmentName VARCHAR(20),
+ HOD VARCHAR(20)
 );
-
-
--- Insert Sample Records
-INSERT INTO Department VALUES
-(101,'Computer Science','Dr.Ravi'),
-(102,'Commerce','Dr.Kumar'),
-(103,'Mathematics','Dr.Sudha');
-
-
--- Display Records
-SELECT * FROM Department;
